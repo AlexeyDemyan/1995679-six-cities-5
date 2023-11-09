8cb1e6ad8cc186f4196cc72b6c2dc1e029d83e34
Alexey Demyanov <deal110988@him-mail.ch>
2023-11-09 17:35:49 +0100

    up until 3.7

---
+++ src/shared/libs/logger/pino.logger.ts
@@ -0,0 +1,29 @@
+import { Logger as PinoInstance, pino } from "pino";
+import { Logger } from "./logger.interface.js";
+
+export class PinoLogger implements Logger {
+  private readonly logger: PinoInstance;
+
+  constructor() {
+    this.logger = pino();
+  }
+
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+
+
+  public warn(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+
+
+  public error(message: string, error: Error, ...args: unknown[]): void {
+    this.logger.info(message, error, ...args);
+  }
+
+
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+}
