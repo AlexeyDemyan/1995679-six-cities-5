87a6a1e3818fc4696b74a2c0ee4354003990dd64
Alexey Demyanov <deal110988@him-mail.ch>
2023-12-05 16:32:42 +0100

    finished 3.1

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,13 +1,17 @@
+import { inject, injectable } from "inversify";
 import { Logger } from "../shared/libs/logger/index.js";
 import { Config, RestSchema } from "../shared/libs/config/index.js";
+import { Component } from "../shared/types/index.js";
 
+@injectable()
 export class RestApplication {
   constructor(
-    private readonly logger: Logger,
-    private readonly config: Config<RestSchema>
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.Config) private readonly config: Config<RestSchema>
   ) {}
 
   public async init() {
+    console.log(`${this.config.get("SALT")}`)
     this.logger.info("Application initiated");
     this.logger.info(`Get value from env $PORT: ${this.config.get("PORT")}`);
   }
