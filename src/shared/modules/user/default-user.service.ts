8d9fb522c46b09777958272a161607a1f30e644c
Alexey Demyanov <deal110988@him-mail.ch>
2024-02-06 17:07:15 +0100

    working on 4.25

---
+++ src/shared/modules/user/default-user.service.ts
@@ -0,0 +1,16 @@
+import { UserService } from "./user-service.interface.js";
+import { DocumentType } from "@typegoose/typegoose";
+import { UserEntity, UserModel } from "./user.entity.js";
+import { CreateUserDTO } from "./dto/create-user.dto.js";
+
+export class DefaultUserService implements UserService {
+  public async create(
+    dto: CreateUserDTO,
+    salt: string
+  ): Promise<DocumentType<UserEntity>> {
+    const user = new UserEntity(dto);
+    user.setPassword(dto.password, salt);
+
+    return UserModel.create(user);
+  }
+}
---
+++ src/shared/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUserDTO {
+  public email: string;
+  public avatarPath: string;
+  public firstname: string;
+  public lastname: string;
+  public password: string;
+}
