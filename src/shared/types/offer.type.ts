aa94e9f9f206fbe1ae11241b2cca47bfdd61798b
Alexey Demyanov <deal110988@him-mail.ch>
2024-02-15 15:17:29 +0100

    finished module4

--- src/shared/types/offer.type.ts
+++ src/shared/types/offer.type.ts
@@ -1,24 +1,14 @@
-import { OfferCity } from "./offer-city.enum.js";
-import { OfferType } from "./offer-type.enum.js";
-// import { OfferFacilities } from "./offer-facilities.enum.js";
-import { Coordinates } from "./coordinates.type.js";
+import { OfferType } from './offer-type.enum.js';
+import { Category } from './category.type.js';
+import { User } from './user.type.js';
 
 export type Offer = {
   title: string;
   description: string;
   postDate: Date;
-  city: OfferCity;
-  imagePreview: string;
-  accommodationPhotos: string[];
-  isPremium: string;
-  isFavorite: string;
-  rating: number;
-  type: OfferType;
-  rooms: number;
-  guests: number;
+  image: string;
+  type: OfferType
   price: number;
-  facilities: string[];
-  author: string;
-  comments: number;
-  coordinates: Coordinates;
+  categories: Category[];
+  user: User;
 }
