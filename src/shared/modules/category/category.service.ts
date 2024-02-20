52ec60bcbaa7d91a737d56aca6739359069a0c82
Alexey Demyanov <deal110988@him-mail.ch>
2024-02-20 17:18:43 +0100

    working on 5.8

--- src/shared/modules/category/category.service.ts
+++ src/shared/modules/category/category.service.ts
@@ -45,4 +45,8 @@ export class DefaultCategoryService implements CategoryService {
 
     return this.create(dto);
   }
+
+  public async find(): Promise<DocumentType<CategoryEntity>[]> {
+    return this.categoryModel.find();
+  }
 }
