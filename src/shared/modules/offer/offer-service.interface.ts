1dc4f986ae1b344d90b46f584f9b95449f20b1a1
Alexey Demyanov <deal110988@him-mail.ch>
2024-02-21 13:40:46 +0100

    working on 5.11

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,8 +1,23 @@
 import { CreateOffetDto } from "./dto/create-offer.dto.js";
 import { DocumentType } from "@typegoose/typegoose";
 import { OfferEntity } from "./offer.entity.js";
+import { UpdateOfferDto } from "./dto/update-offer.dto.js";
 
 export interface OfferService {
   create(dto: CreateOffetDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(
+    offerId: string,
+    dto: UpdateOfferDto
+  ): Promise<DocumentType<OfferEntity> | null>;
+  findByCaterogyId(
+    categoryId: string,
+    count?: number
+  ): Promise<DocumentType<OfferEntity>[]>;
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findNew(count: number): Promise<DocumentType<OfferEntity>[]>;
+  findDiscussed(count: number): Promise<DocumentType<OfferEntity>[]>;
+  exists(documentId: string): Promise<boolean>;
 }
